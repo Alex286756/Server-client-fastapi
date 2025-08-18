61ef123f4a61d833d4685805227f3d3bbd237cc5
kuksov aleksej <a.g.kuksov@yandex.ru>
2025-08-18 13:56:27 +0300

    Add exception handling

--- Server/routers/record_router.py
+++ Server/routers/record_router.py
@@ -1,6 +1,8 @@
 import math
 from typing import Any
 from fastapi import APIRouter, HTTPException
+from pydantic.error_wrappers import ValidationError
+from sqlalchemy.exc import IntegrityError, DatabaseError
 
 from Server.models.record_model import RecordCreate, RecordBase
 from Server.repositories.my_db import SessionLocal
@@ -15,12 +17,20 @@ async def create(record: RecordCreate) -> Any:
     :param record: Данные новой записи
     :return: Информация о новой записи
     """
-    new_record = RecordBase(**record.model_dump())
-    with SessionLocal() as session:
-        session.add(new_record)
-        session.commit()
-        session.refresh(new_record)
+    try:
+        new_record = RecordBase(**record.model_dump())
+        with SessionLocal() as session:
+            session.add(new_record)
+            session.commit()
+            session.refresh(new_record)
         return new_record
+    except IntegrityError as database_error:
+        session.rollback()
+        raise HTTPException(status_code=400, detail=f'Нарушение целостности данных: {database_error}')
+    except ValidationError as value_error:
+        raise HTTPException(status_code=422, detail=str(value_error))
+    except Exception as exception:
+        raise HTTPException(status_code=500, detail=f'Ошибка сервера: {exception}')
 
 
 @router.get('/list')
@@ -31,18 +41,25 @@ async def list_all(page: int = 1, per_page: int = 10):
     :param per_page: Количество записей на странице
     :return: Список элементов
     """
-    if per_page < 1:
-        raise HTTPException(status_code=400, detail="Количество элементов на страницу не может быть меньше 1")
-    if page < 1:
-        raise HTTPException(status_code=400, detail="Номер страницы не может быть меньше 1")
+    try:
+        if per_page < 1:
+            raise HTTPException(status_code=400, detail="Количество элементов на страницу не может быть меньше 1")
+        if page < 1:
+            raise HTTPException(status_code=400, detail="Номер страницы не может быть меньше 1")
 
-    with SessionLocal() as session:
-        skip = (page - 1) * per_page
-        total_count = session.query(RecordBase).count()
-        max_pages = math.ceil(total_count / per_page) if total_count > 0 else 1
+        with SessionLocal() as session:
+            skip = (page - 1) * per_page
+            total_count = session.query(RecordBase).count()
+            max_pages = math.ceil(total_count / per_page) if total_count > 0 else 1
 
-        if page > max_pages:
-            raise HTTPException(status_code=400, detail="Указан неверный номер страницы")
+            if page > max_pages:
+                raise HTTPException(status_code=400, detail="Указан неверный номер страницы")
 
-        result = session.query(RecordBase).offset(skip).limit(per_page).all()
-        return result
+            result = session.query(RecordBase).offset(skip).limit(per_page).all()
+            return result
+    except DatabaseError as database_error:
+        raise HTTPException(status_code=500, detail=f'Ошибка при обращении к базе данных: {database_error}')
+    except HTTPException as http_exception:
+        raise http_exception
+    except Exception as exception:
+        raise HTTPException(status_code=500, detail=f'Ошибка сервера: {exception}')
